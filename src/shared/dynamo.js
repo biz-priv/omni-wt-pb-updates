05a43d2a48ce8d79cd67d175a86844aedb712ece
tharun kancham <tharun.kncham@bizcloudexperts.com>
2024-05-10 10:32:04 -0500

    test

--- src/shared/dynamo.js
+++ src/shared/dynamo.js
@@ -2,45 +2,58 @@ const AWS = require('aws-sdk');
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 const _ = require('lodash');
 
+const { ORDERS_TABLE_NAME, MOVEMENT_ORDER_TABLE_NAME, ADD_MILESTONE_TABLE_NAME } = process.env;
+
 async function query(params) {
-    try {
-        return await dynamoDB.query(params).promise();
-    } catch (error) {
-        console.error('Query Item Error:', error, '\nQuery Params:', params);
-        throw error
+    async function helper(params) {
+        try {
+            const result = await dynamoDB.query(params).promise();
+            let data = _.get(result, 'Items', []);
+
+            if (result.LastEvaluatedKey) {
+                params.ExclusiveStartKey = result.LastEvaluatedKey;
+                data = data.concat(await helper(params));
+            }
+
+            return data;
+        } catch (error) {
+            console.error('Query Item Error:', error, '\nQuery Params:', params);
+            throw error;
+        }
     }
+
+    return helper(params);
 }
 
 async function getMovementOrder(id) {
     const movementParams = {
-        TableName: 'omni-pb-rt-movement-order',
+        TableName: MOVEMENT_ORDER_TABLE_NAME,
         IndexName: 'movement_id-index',
         KeyConditionExpression: 'movement_id = :movement_id',
         ExpressionAttributeValues: {
             ':movement_id': id
         }
     };
-    
+
     try {
-        const result = await query(movementParams);
-        const items = _.get(result, 'Items', []);
+        const items = await query(movementParams);
 
         if (items.length > 0) {
             const orderId = _.get(items, '[0]order_id');
             console.info('Order ID:', orderId);
             return orderId;
         } else {
-            throw new Error('No Record found in Movement order Dynamo Table for Id:',id)
+            throw new Error('No Record found in Movement order Dynamo Table for Id:', id);
         }
     } catch (error) {
         console.error('Error in getMovementOrder function:', error);
-        throw error
+        throw error;
     }
 }
 
 async function getOrder(orderId) {
     const orderParams = {
-        TableName: 'omni-pb-rt-orders-dev',
+        TableName: ORDERS_TABLE_NAME,
         KeyConditionExpression: 'id = :id',
         ExpressionAttributeValues: {
             ':id': orderId
@@ -50,35 +63,33 @@ async function getOrder(orderId) {
     console.log('Fetching data from:', orderParams.TableName);
 
     try {
-        const result = await query(orderParams);
-        const items = _.get(result, 'Items', []);
+        const items = await query(orderParams);
 
         if (items.length > 0) {
             const housebillNum = _.get(items, '[0]blnum');
             console.info('Housebill Number:', housebillNum);
             return housebillNum;
         } else {
-            throw new Error('No Record found in Orders Dynamo Table for orderId:',orderId)
+            throw new Error('No Record found in Orders Dynamo Table for orderId:', orderId);
         }
     } catch (error) {
         console.error('Error in getOrder function:', error);
-        throw error
+        throw error;
     }
 }
 
-async function updateMilestone(finalPayload){
+async function updateMilestone(finalPayload) {
     const addMilestoneParams = {
-        TableName: 'omni-pb-214-add-milestone',
+        TableName: ADD_MILESTONE_TABLE_NAME,
         Item: finalPayload
     };
     try {
         await dynamoDB.put(addMilestoneParams).promise();
         console.log('Successfully inserted payload into omni-pb-214-add-milestone table');
     } catch (error) {
-        console.error('Error while udating data into 214-add-milestone Dynamo Table',error)
-        throw error
+        console.error('Error while updating data into 214-add-milestone Dynamo Table', error);
+        throw error;
     }
-
 }
 
 module.exports = {
